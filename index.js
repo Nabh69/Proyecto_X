ff952789e51f697a0d15d73c6c92ef6eb2fca03d
Nabh69 <167888476+Nabh69@users.noreply.github.com>
2024-09-25 17:57:13 -0500

    Update index.js
    Signed-off-by: Nabh69 <167888476+Nabh69@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Gracias por decir que si, Te amo mucho mi niña <3')
+    alert('YA SABÍA GOLOS@')
 });
 
 const noBtn = document.querySelector('#noBtn');
